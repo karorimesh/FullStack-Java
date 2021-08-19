91d5715720eb6001fc6a14119a85b4295bba6318
karorimesh <karorimesh@gmail.com>
2021-08-19 15:58:35 +0300

    Fullstack Java

---
+++ src/AgeAdult.java
@@ -0,0 +1,10 @@
+public class AgeAdult {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age < 18){
+            System.out.println("Under 18");
+        }else {
+            System.out.println("Adult");
+        }
+    }
+}

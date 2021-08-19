d84bfaf79999225f48d170829f9abd217c164e15
karorimesh <karorimesh@gmail.com>
2021-08-19 16:23:12 +0300

    Fullstack 1.1

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Enter a sentence: ");
-        Scanner hello = new Scanner(System.in);
-        String Hello =hello.nextLine();
-        System.out.println("You typed =>"+Hello);
-    }
-}
--- src/VariableDemo.java
+++
@@ -1,7 +0,0 @@
-public class VariableDemo {
-    public static void main(String[] args) {
-        int $number;
-        $number = 10;
-        $number = $number+10;
-    }
-}
